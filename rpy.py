1593c1ca8b087e5306f28716040e93abc6e27f7b
campos0170 <154468247+campos0170@users.noreply.github.com>
2024-11-30 05:32:53 -0500

    Update rpy.py

--- rpy.py
+++ rpy.py
@@ -1,44 +1,39 @@
 from random import choice
 
-
 def play():
-
     total_games = 0
     player_wins = 0
     computer_wins = 0
-  
+
     while True:
-
-        user = input("Please select 'r' for rock, 's' for scissor and, 'p' for paper, q for quit if you no longer want to play: ")
+        user = input("Please select 'r' for rock, 's' for scissor, 'p' for paper, or 'q' to quit: ").lower()
         
-        if user == 'q'.lower():
-            print(f"Thanks for playing! You won a total of '{player_wins}' out of {total_games} games.")
-
+        if user == 'q':
+            print(f"Thanks for playing! You won {player_wins} out of {total_games} games.")
             break
 
-        choices = ['r','p','r']
-
+        choices = ['r', 'p', 's']
         computer = choice(choices)
 
-        # The following if ststement determines if we have a tie in the game, meaning both the computer and
-        # user made the same choice.
-
         if user not in choices:
-            print("Please enter a valid option")
+            print("Invalid option! Please select 'r', 'p', or 's'.")
+            continue
 
-        elif user == computer:
-            print( f"You and the computer both selected '{user}', we have a Tie!")
-            total_games+=1
+        print(f"You chose: {user}, Computer chose: {computer}")
 
-        elif (user == 'r' and computer == 'p') or (user == 's' and computer == 'p') or (user == 'p' and computer == 'r'):
+        if user == computer:
+            print(f"It's a tie! Both chose '{user}'.")
+        elif (user == 'r' and computer == 's') or \
+             (user == 's' and computer == 'p') or \
+             (user == 'p' and computer == 'r'):
             print("Congratulations! You won!")
-            player_wins +=1
-            total_games+=1
-        
+            player_wins += 1
         else:
-            
-            print( "You lost")
-            computer_wins+=1
-            total_games+=1
+            print("You lost this round.")
+            computer_wins += 1
+
+        total_games += 1
+
+# Run the game
 play()
 
